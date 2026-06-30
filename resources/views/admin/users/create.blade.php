5e1650f26c8e49757d46e5ca51f0a52cda81c1e1
aiken214 <paul.arsolon@deped.gov.ph>
2026-06-30 14:59:45 +0800

    fix user create and table view

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -65,20 +65,30 @@
     </div>
     <div class="card-body">
 
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <form method="POST" action="{{ route('admin.users.store') }}">
             @csrf
-           
+
 
             <div class="row">
                 <div class="col-sm-6">
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" name="name"  required>
+                        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
                     </div>
         
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email"  required>
+                        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
                     </div>
         
                     <div class="mb-3">
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -66,6 +66,16 @@
     </div>
     <div class="card-body">
 
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <form method="POST" action="{{ route('admin.users.update', $user->id) }}">
             @csrf
             @method('PATCH')
